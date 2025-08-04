f99fdbdadd0a3361a4dfd215e3ad1cd1108feb43
archuser-q <availableemailaddress123@tutamail.com>
2025-08-04 20:22:08 +0700

    adding routing path

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,21 +1,23 @@
 import { AppstoreOutlined, CompassOutlined, ContactsOutlined, DockerOutlined, HomeOutlined, KubernetesOutlined, LogoutOutlined, UserOutlined } from "@ant-design/icons";
 import { Menu, type MenuProps } from "antd"
+import { useNavigate } from '@tanstack/react-router'
+import { keyToPathMap } from '@/configuration/routing/routing'
 
 type MenuItem = Required<MenuProps>['items'][number];
 
 const items: MenuItem[] = [
-    {key:1, label: 'Tổng quan', icon:<HomeOutlined/>},
-    {key:2, label:'Tàu cá', icon:<DockerOutlined/>, children:[
+    {key:'1', label: 'Tổng quan', icon:<HomeOutlined/>},
+    {key:'2', label:'Tàu cá', icon:<DockerOutlined/>, children:[
         {key:'2-1',label:'Trong nước'},
         {key:'2-2',label:'Ngoài nước'}
     ]},
-    {key:3, label: 'Cảng cá', icon:<KubernetesOutlined />},
-    {key:4, label:'Danh sách thuyền viên', icon:<ContactsOutlined />},
-    {key:5, label:'Tài khoản', icon:<UserOutlined />, children:[
+    {key:'3', label: 'Cảng cá', icon:<KubernetesOutlined />},
+    {key:'4', label:'Danh sách thuyền viên', icon:<ContactsOutlined />},
+    {key:'5', label:'Tài khoản', icon:<UserOutlined />, children:[
         {key:'5-1',label:'Trong nước'},
         {key:'5-2',label:'Ngoài nước'}
     ]},
-    {key:6, label:'Danh mục', icon:<AppstoreOutlined/>, children:[
+    {key:'6', label:'Danh mục', icon:<AppstoreOutlined/>, children:[
         {key:'6-1',label:'Ngành nghề'},
         {key:'6-2',label:'Loài cá'},
         {key:'6-3',label:'Ngư trường'}
@@ -29,10 +31,18 @@ const items: MenuItem[] = [
 ]
 
 const Sidebar: React.FC = () => {
+    const navigate = useNavigate();
+    const handleClick: MenuProps['onClick'] = ({key})=> {
+        const path = keyToPathMap[key];
+        if (path){
+            navigate({to: path});
+        }
+    }
     return(
         <Menu
         mode="inline"
-        items={items}/>
+        items={items}
+        onClick={handleClick}/>
     );
 }
 
